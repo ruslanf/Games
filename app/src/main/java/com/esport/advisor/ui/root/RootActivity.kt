abd5720dc3b4abbefeaeb5808760603e28097bf6
Ruslan PC <Zerg>
2019-06-07 20:17:33 +0400

    Added drawer layout

--- app/src/main/java/com/esport/advisor/ui/root/RootActivity.kt
+++ app/src/main/java/com/esport/advisor/ui/root/RootActivity.kt
@@ -2,15 +2,21 @@ package com.esport.advisor.ui.root
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.esport.advisor.R
 import com.esport.advisor.root.BackPressedInterface
 import com.esport.advisor.root.Screens
+import com.google.android.material.navigation.NavigationView
 import org.koin.android.ext.android.inject
 import ru.terrakok.cicerone.NavigatorHolder
 import ru.terrakok.cicerone.Router
 import ru.terrakok.cicerone.android.support.SupportAppNavigator
 
-class RootActivity : AppCompatActivity() {
+class RootActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private val router by inject<Router>()
     private val navigatorHolder by inject<NavigatorHolder>()
@@ -19,7 +25,19 @@ class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_root)
+        setContentView(R.layout.fragment_drawer_layout)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
+
         if (savedInstanceState == null) {
             router.newRootScreen(Screens.MainScreen)
         }
@@ -36,9 +54,24 @@ class RootActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
         when (val supportFM = supportFragmentManager.findFragmentById(R.id.flRoot)) {
             is BackPressedInterface -> when (supportFM.onBackPressed()) { false -> router.exit() }
             else -> router.exit()
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
